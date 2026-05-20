8ed7c00fc685ebeaf1c890f9af451b6434d600a0
yonibzy <yonibzy@gmail.com>
2026-05-20 17:23:43 +0200

    Update Pathfinder.java

--- src/main/java/exit/simulation/Pathfinder.java
+++ src/main/java/exit/simulation/Pathfinder.java
@@ -1,5 +1,9 @@
 package exit.simulation;
 
+/**
+ * Provides pathfinding algorithms (Dijkstra) for agent navigation.
+ * @author Lassiana
+ */
 public class Pathfinder {
-    // TODO - Lassina
+    // TODO - Lassiana
 }
