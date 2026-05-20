ccf18550410453810ffd6a5bcb747504bd89de3c
yonibzy <yonibzy@gmail.com>
2026-05-20 17:21:16 +0200

    Update SimulationEngine.java

--- src/main/java/exit/simulation/SimulationEngine.java
+++ src/main/java/exit/simulation/SimulationEngine.java
@@ -1,5 +1,9 @@
 package exit.simulation;
 
+/**
+ * Controls the simulation loop and manages agent movements at each tick.
+ * @author Yoni
+ */
 public class SimulationEngine {
-    // TODO - Yoni + Clement
+    // TODO - Yoni
 }
