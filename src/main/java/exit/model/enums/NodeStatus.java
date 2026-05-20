f572176d34288eae8f52ceebecb058b97fe344ea
yonibzy <yonibzy@gmail.com>
2026-05-20 17:16:27 +0200

    Update NodeStatus.java

--- src/main/java/exit/model/enums/NodeStatus.java
+++ src/main/java/exit/model/enums/NodeStatus.java
@@ -1,10 +1,12 @@
 package exit.model.enums;
+
 /**
- * Represents the status of a node in the graph.
+ * Represents the accessibility status of a node.
+ * @author Yoni
  */
 public enum NodeStatus {
-    /** Node is accessible. */
-    OPEN, 
-    /** Node is blocked (fire door, smoke...). */
+    /** Node is accessible and open. */
+    OPEN,
+    /** Node is blocked (fire door, smoke, debris...). */
     BLOCKED
 }
