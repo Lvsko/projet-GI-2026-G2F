41cc27d5cf03e2859cf62835f8a589b6a9bcd802
yonibzy <yonibzy@gmail.com>
2026-05-26 10:19:43 +0200

    Update Agent.java

--- src/main/java/exit/model/Agent.java
+++ src/main/java/exit/model/Agent.java
@@ -1,5 +1,8 @@
 package exit.model;
 
+import exit.model.enums.AgentBehavior;
+import exit.model.enums.AgentState;
+import exit.model.enums.AgentType;
 import java.util.Objects;
 
 /**
@@ -12,15 +15,26 @@ public class Agent {
     private Node currentNode;
     private Edge currentEdge;
     private Node destinationNode;
-    
+    private float maxSpeed;
+    private AgentState state;
+    private AgentBehavior behavior;
+    private AgentType type;
+    private float densityTolerance;
+
     private static int numberAgent = 0;
 
     /**
      * Creates an agent with an identifier and a starting node.
      * @param id          unique identifier of the agent
      * @param startNode   initial node of the agent
+	 * @param destinationNode target exit node
+     * @param maxSpeed maximum speed of the agent
+     * @param state initial psychological state
+     * @param behavior movement behavior
+     * @param type physical profile
+     * @param densityTolerance tolerance to crowded areas
      */
-    public Agent(String id, Node startNode, Node destinationNode) {
+    public Agent(String id, Node startNode, Node destinationNode, float maxSpeed, AgentState state, AgentBehavior behavior, AgentType type, float densityTolerance ) {
         if (id == null || id.isBlank()) {
             throw new IllegalArgumentException("Identifier can't be empty");
         }
@@ -28,6 +42,12 @@ public class Agent {
         this.id = id;
         this.currentNode = startNode;
         this.currentEdge = null;
+		this.destinationNode = destinationNode;
+		this.maxSpeed = maxSpeed;
+        this.state = state;
+        this.behavior = behavior;
+        this.type = type;
+        this.densityTolerance = densityTolerance;
         if (startNode != null) {
             startNode.addAgent(this);
         }
@@ -38,7 +58,7 @@ public class Agent {
      * @param startNode   initial node of the agent
      */
     public Agent(Node startNode, Node destinationNode) {
-    	this("agent"+numberAgent, startNode, destinationNode);
+    	this("agent"+numberAgent, startNode, destinationNode, 1.0f, AgentState.CALM, AgentBehavior.COOPERATIVE, AgentType.ADULT, 0.5f);
     }
     
     /** 
@@ -59,6 +79,16 @@ public class Agent {
     
     public Node getDestinationNode() {
 		return destinationNode;
+	
+	public float getMaxSpeed() { return maxSpeed; }
+    public AgentState getState() { return state; }
+    public void setState(AgentState state) { this.state = state; }
+    public AgentBehavior getBehavior() { return behavior; }
+    public AgentType getType() { return type; }
+    public float getDensityTolerance() { return densityTolerance; }
+	
+	
+	
 	}
 
 	/**
