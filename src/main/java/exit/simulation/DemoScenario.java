386f9d74e18a46fd85458946e96e1153fbc7a4a2
yonibzy <yonibzy@gmail.com>
2026-05-29 10:04:45 +0200

    Create DemoScenario.java

---
+++ src/main/java/exit/simulation/DemoScenario.java
@@ -0,0 +1,9 @@
+package exit.simulation;
+/**
+ * Provides predefined simulation scenarios for the demo mode.
+ * Includes best case, average case and worst case scenarios.
+ * @author Clement
+ */
+public class DemoScenario {
+    // TODO - Clement
+}
