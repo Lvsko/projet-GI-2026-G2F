b1f6a5213d97cc14eba345c50151dd568b989c1c
yonibzy <yonibzy@gmail.com>
2026-05-29 10:02:19 +0200

    Delete exit/simulation/DemoScenario.java

--- exit/simulation/DemoScenario.java
+++
@@ -1,9 +0,0 @@
-package exit.simulation;
-/**
- * Provides predefined simulation scenarios for the demo mode.
- * Includes best case, average case and worst case scenarios.
- * @author Clement
- */
-public class DemoScenario {
-    // TODO - Clement
-}
