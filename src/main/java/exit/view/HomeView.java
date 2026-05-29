354c601eef17f44cada0479489de4af8c38958f1
yonibzy <yonibzy@gmail.com>
2026-05-29 10:03:10 +0200

    Create HomeView.java

---
+++ src/main/java/exit/view/HomeView.java
@@ -0,0 +1,9 @@
+package exit.view;
+/**
+ * Home screen of the EXIT application.
+ * Displays the main menu with Demo and Manual Configuration options.
+ * @author Yoni
+ */
+public class HomeView {
+    // TODO - Yoni
+}
