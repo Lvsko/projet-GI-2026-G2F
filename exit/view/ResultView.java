22176a500464a22e582d90c6b5beaf8068421172
yonibzy <yonibzy@gmail.com>
2026-05-29 09:59:50 +0200

    Create ResultView.java

---
+++ exit/view/ResultView.java
@@ -0,0 +1,9 @@
+package exit.view;
+/**
+ * Displays the results of a simulation.
+ * Shows evacuation time, evacuated agents and congested edges.
+ * @author Leonardo
+ */
+public class ResultView {
+    // TODO - Leonardo
+}
