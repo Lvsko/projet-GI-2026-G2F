a6842e29f5ecb0ce0513d34b70485f32170dd666
yonibzy <yonibzy@gmail.com>
2026-05-29 10:04:16 +0200

    Create ConfigView.java

---
+++ src/main/java/exit/view/ConfigView.java
@@ -0,0 +1,9 @@
+package exit.view;
+/**
+ * Manual configuration screen.
+ * Allows the user to build a custom graph and launch a simulation.
+ * @author Ruben
+ */
+public class ConfigView {
+    // TODO - Ruben
+}
