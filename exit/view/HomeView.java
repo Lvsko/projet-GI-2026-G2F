67057d16c318c7e999728aff3443ff2076d2053c
yonibzy <yonibzy@gmail.com>
2026-05-29 10:02:31 +0200

    Delete exit/view/HomeView.java

--- exit/view/HomeView.java
+++
@@ -1,9 +0,0 @@
-package exit.view;
-/**
- * Home screen of the EXIT application.
- * Displays the main menu with Demo and Manual Configuration options.
- * @author Yoni
- */
-public class HomeView {
-    // TODO - Yoni
-}
